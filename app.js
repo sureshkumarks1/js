7c9bc43e38ec0682a482c937cbd6924c690ccbec
SURESH KUMAR K S <sureshkumarks1@gmail.com>
2023-10-18 07:38:22 +0530

    updated files index app.js style.css

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-let names = ["suresh", "manu", "sanu"];
 
-let kitchenitemselect = document.querySelector(".kitchenitemlist");
-let allli = document.querySelectorAll("li");
+let kitchenItemSelect = document.querySelector(".kitchenitemlist");
+let kitchenInput = document.querySelector(".kitcheninput");
+let submit = document.querySelector(".submit");
+
+function valueFromDom(){
+    let li = kitchenInput.value;
+    
+    console.log(li);
+
+}
+
+submit.addEventListener("click", valueFromDom);
+
+
+
+
 //console.log(allli);
 // allli[0].innerText = names[0];
 /*
@@ -12,9 +25,3 @@ for(let powder of names){
     console.log(powder);
 }
 */
-names.forEach(function(powder){
-    let li = document.createElement("li");
-    li.innerText = powder;
-    kitchenitemselect.appendChild(li);
-    // console.log(powder);
-});
